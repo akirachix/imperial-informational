253f0cc5f76941075872f0be5dc3d88b6f66fb8c
Bright Joy Karanja <brightjoykaranja@gmail.com>
2025-06-20 11:46:09 +0300

    create the html, css, and javascript files for the contact page

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-const learnBtn = document.getElementById('learn-button').addEventListener('click', function(){
-    document.getElementById('aboutUs').scrollIntoView({behavior: 'smooth'});
-});
-const learnButton = document.getElementById('learn-more-btn').addEventListener('click', function(){
-    document.getElementById('home').scrollIntoView({behavior: 'smooth'});
-});
-const explore= document.getElementById('explore').addEventListener('click' , function(){
-    document.getElementById('exploreMore').scrollIntoView({behavior: 'smooth'});
-});
-const aboutUs= document.getElementById('aboutUs').addEventListener('click' , function(){
-    document.getElementById('moreAboutUs').scrollIntoView({behavior: 'smooth'});
-});
-const contactUsNow= document.getElementById('contactUs').addEventListener('click' , function(){
-    document.getElementById('contactUsNow').scrollIntoView({behavior: 'smooth'});
-});
-
-
 const form = document.querySelector('form#contactForm');
 if (form) {
   form.addEventListener('submit', (e) => {
